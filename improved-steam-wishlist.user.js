e450ce8c8bbceb45db07eca3ce4ba6b7f6e10c75
Xiyng <xiyng.dev@gmail.com>
2019-01-29 21:58:01 +0200

    Add the basic script
    Initially, the only difference compared to the default list is that this
    userscript loads all wishlist elements when opening the wishlist. Images
    are loaded only when needed, which ensures decent performance (i.e. I'm
    not noticing any difference personally).

---
+++ improved-steam-wishlist.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name     Improved Steam Wishlist
+// @version  0.1
+// @author   Xiyng
+// @include  https://store.steampowered.com/wishlist/profiles/*
+// @run-at   document-start
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+"use strict";
+
+const codeToInject =
+`// Code here relies on original wishlist code by Valve.
+
+CWishlistController.prototype.OriginalOnScroll = CWishlistController.prototype.OnScroll;
+
+CWishlistController.prototype.OnScroll = function() {
+    this.LoadAllElementsWithoutImages();
+    return this.OriginalOnScroll.apply(this, arguments);
+};
+
+CWishlistController.prototype.LoadAllElementsWithoutImages = function() {
+    for (let i = 0; i < this.rgVisibleApps.length; i++) {
+        this.LoadElementWithoutImages(i);
+    }
+};
+
+// This is pretty much LoadElement with the images part removed.
+CWishlistController.prototype.LoadElementWithoutImages = function(nIndex) {
+    var unAppId = this.rgVisibleApps[nIndex];
+    var $elTarget = this.rgElements[unAppId];
+
+    if (!this.rgLoadedApps.includes(unAppId)) {
+        this.rgLoadedApps.push(unAppId);
+        this.elContainer.append($elTarget);
+    }
+
+    if(!$elTarget.hasClass('dragging')) {
+				$elTarget.css('top', nIndex * this.nRowHeight);
+    }
+};
+
+CWishlistController.prototype.UnloadElement = () => {};`;
+
+function injectScript(sourceCode) {
+  	const $script = $("<script></script>");
+    $script.prop("type", "text/javascript");
+    $script.text(sourceCode);
+    $("head").append($script);
+}
+
+$(window).on("afterscriptexecute", event => {
+    const scriptSource = event.target.src;
+    if (scriptSource.indexOf("wishlist.js") >= 0) {
+        console.log("Found wishlist.js");
+        // We need to inject some code to override default wishlist behaviour.
+        injectScript(codeToInject);
+    }
+});
